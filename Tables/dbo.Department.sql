178137cd59ce0cd915bf793039bc7e3f5c734cbc
godfrey dsouza <godfreyadsouza@outlook.com>
2021-05-10 08:21:57 +0100

    save changes

--- Tables/dbo.Department.sql
+++ Tables/dbo.Department.sql
@@ -1,10 +1,3 @@
-
-/****** Object:  Table [dbo].[Department]    Script Date: 29/12/2018 14:25:18 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
 IF EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID(N'[Department]') AND type IN (N'U')) 
 BEGIN 
    DROP TABLE [Department] 
